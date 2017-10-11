c20bf2073270a2c18624d1c18a974a48bdbd7184
Pettyfer <pettyferlove@live.cn>
2017-10-11 20:55:20 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import 'font-awesome/css/font-awesome.css'
 import '../theme/index.css'
 import './my-theme/petty.less'
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,11 +8,11 @@
     </el-row>
     <el-row class="main">
       <el-row type="flex">
-        <el-menu default-active="2" class="sidebar" theme="dark" collapse="true">
-          <el-submenu index="1">
+        <el-menu default-active="1" class="sidebar" theme="dark" collapse="true">
+          <!--<el-submenu index="1">
             <template slot="title">
-              <i class="el-icon-message"></i>
-              <span slot="title">导航一</span>
+              <i class="fa fa-dashboard"></i>
+              <span slot="title">仪表盘</span>
             </template>
             <el-menu-item-group>
               <span slot="title">分组一</span>
@@ -26,15 +26,69 @@
               <span slot="title">选项4</span>
               <el-menu-item index="1-4-1">选项1</el-menu-item>
             </el-submenu>
-          </el-submenu>
+          </el-submenu>-->
+          <el-menu-item index="1">
+            <i class="fa fa-dashboard"></i>
+            <span slot="title">仪表盘</span>
+          </el-menu-item>
           <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+            <i class="fa fa-cogs"></i>
+            <span slot="title">小组件</span>
           </el-menu-item>
           <el-menu-item index="3">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航三</span>
+            <i class="fa fa-database"></i>
+            <span slot="title">面板</span>
           </el-menu-item>
+          <el-menu-item index="4">
+            <i class="fa fa-pencil"></i>
+            <span slot="title">编辑器</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="fa fa-photo"></i>
+            <span slot="title">图片列表</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <i class="fa fa-bar-chart"></i>
+            <span slot="title">图表</span>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <i class="fa fa-anchor"></i>
+            <span slot="title">UI元素</span>
+          </el-menu-item>
+
+          <el-submenu index="8">
+            <template slot="title">
+              <i class="fa fa-database"></i>
+              <span slot="title">数据表格</span>
+            </template>
+            <el-menu-item index="8-1">
+              <i class="fa fa-hdd-o"></i>
+              <span slot="title">基础表格</span>
+            </el-menu-item>
+            <el-menu-item index="8-2">
+              <i class="fa fa-bars"></i>
+              <span slot="title">可编辑表格</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="9">
+            <template slot="title">
+              <i class="fa fa-file"></i>
+              <span slot="title">页面</span>
+            </template>
+            <el-menu-item index="9-1">
+              <i class="fa fa-mouse-pointer"></i>
+              <span slot="title">登录页</span>
+            </el-menu-item>
+            <el-menu-item index="9-2">
+              <i class="fa fa-cogs"></i>
+              <span slot="title">锁屏</span>
+            </el-menu-item>
+            <el-menu-item index="9-3">
+              <i class="fa fa-minus"></i>
+              <span slot="title">加载中</span>
+            </el-menu-item>
+          </el-submenu>
         </el-menu>
       </el-row>
     </el-row>
