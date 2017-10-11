dee7627830267d93d94252407616e6c2c8f8a6cd
Pettyfer <pettyferlove@live.cn>
2017-10-11 19:54:53 +0800

    commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <el-row>
+      <el-col :span="24">
+        <div class="logo">
+          <img src="../assets/logo.png" height="30" alt="">
+          <span>VUE</span>PettyDev
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+  .main-footer{
+    margin-bottom: 5px;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
