c1e1b6493673c11b2352de1850031b57207819ef
Pettyfer <pettyferlove@live.cn>
2017-10-19 21:45:14 +0800

    :tada:Router异步加载

---
+++ src/components/user/vp-user-panel.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-dropdown trigger="click" class="vp-user-panel" menu-align="end">
+    <el-button>
+      <img :src="adminInfo.image" height="30" alt=""><span>{{adminInfo.name}}</span>
+    </el-button>
+    <el-dropdown-menu slot="dropdown" class="vp-user-panel-menu">
+      <el-dropdown-item><i class="fa fa-cog"></i>用户设置</el-dropdown-item>
+      <el-dropdown-item divided><router-link to="/login"><i class="fa fa-key"></i>退出系统</router-link></el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<style>
+
+</style>
+
+<script>
+    export default {
+      name: 'VpUserPanel',
+      props: {
+        adminInfo: {
+          type: Object,
+          default: function () {
+            return {
+              image: require('@/assets/img/user-petty.jpg'),
+              name: 'Pettyfer'
+            }
+          }
+        }
+      }
+    }
+</script>
+
