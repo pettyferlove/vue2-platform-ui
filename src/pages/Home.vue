b49215ae36fbc8b2a21f8955a00c187215e30ad9
Pettyfer <pettyferlove@live.cn>
2017-10-29 10:47:10 +0800

    :memo:样式完善

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,87 +16,84 @@
             </div>
           </div>
         </div>
-        <el-menu-item-group>
-          <template slot="title">主菜单</template>
-          <el-menu-item index="/">
-            <i class="fa fa-dashboard"></i>
-            <span slot="title">仪表盘</span>
+        <el-menu-item index="/">
+          <i class="fa fa-dashboard"></i>
+          <span slot="title">仪表盘</span>
+        </el-menu-item>
+        <el-menu-item index="/widget">
+          <i class="fa fa-cogs"></i>
+          <span slot="title">小组件</span>
+        </el-menu-item>
+        <el-menu-item index="/panels">
+          <i class="fa fa-database"></i>
+          <span slot="title">面板</span>
+        </el-menu-item>
+        <el-submenu index="editor">
+          <template slot="title">
+            <i class="fa fa-pencil"></i>
+            <span slot="title">编辑器</span>
+          </template>
+          <el-menu-item index="/quillEditor">
+            <i class="fa fa-pencil"></i>
+            <span slot="title">Quill-Editor</span>
           </el-menu-item>
-          <el-menu-item index="/widget">
-            <i class="fa fa-cogs"></i>
-            <span slot="title">小组件</span>
+          <el-menu-item index="/simplemdeEditor">
+            <i class="fa fa-pencil"></i>
+            <span slot="title">Simplemde</span>
           </el-menu-item>
-          <el-menu-item index="/panels">
+        </el-submenu>
+        <el-menu-item index="/imageList">
+          <i class="fa fa-photo"></i>
+          <span slot="title">图片列表</span>
+        </el-menu-item>
+        <el-menu-item index="/charts">
+          <i class="fa fa-bar-chart"></i>
+          <span slot="title">图表</span>
+        </el-menu-item>
+        <el-submenu index="ui">
+          <template slot="title">
+            <i class="fa fa-anchor"></i>
+            <span slot="title">UI元素</span>
+          </template>
+          <el-menu-item index="/icons">
+            <i class="fa fa-font"></i>
+            <span slot="title">字体图标</span>
+          </el-menu-item>
+          <el-menu-item index="/icons">
+            <i class="fa fa-font"></i>
+            <span slot="title">字体图标</span>
+          </el-menu-item>
+        </el-submenu>
+
+        <el-submenu index="8">
+          <template slot="title">
             <i class="fa fa-database"></i>
-            <span slot="title">面板</span>
+            <span slot="title">数据表格</span>
+          </template>
+          <el-menu-item index="/basicTable">
+            <i class="fa fa-hdd-o"></i>
+            <span slot="title">基础表格</span>
           </el-menu-item>
-          <el-submenu index="editor">
-            <template slot="title">
-              <i class="fa fa-pencil"></i>
-              <span slot="title">编辑器</span>
-            </template>
-            <el-menu-item index="/quillEditor">
-              <i class="fa fa-pencil"></i>
-              <span slot="title">Quill-Editor</span>
-            </el-menu-item>
-            <el-menu-item index="/simplemdeEditor">
-              <i class="fa fa-pencil"></i>
-              <span slot="title">Simplemde</span>
-            </el-menu-item>
-          </el-submenu>
-          <el-menu-item index="/imageList">
-            <i class="fa fa-photo"></i>
-            <span slot="title">图片列表</span>
+          <el-menu-item index="/editableTable">
+            <i class="fa fa-bars"></i>
+            <span slot="title">可编辑表格</span>
           </el-menu-item>
-          <el-menu-item index="/charts">
-            <i class="fa fa-bar-chart"></i>
-            <span slot="title">图表</span>
-          </el-menu-item>
-          <el-submenu index="ui">
-            <template slot="title">
-              <i class="fa fa-anchor"></i>
-              <span slot="title">UI元素</span>
-            </template>
-            <el-menu-item index="/icons">
-              <i class="fa fa-font"></i>
-              <span slot="title">字体图标</span>
-            </el-menu-item>
-            <el-menu-item index="/icons">
-              <i class="fa fa-font"></i>
-              <span slot="title">字体图标</span>
-            </el-menu-item>
-          </el-submenu>
+        </el-submenu>
 
-          <el-submenu index="8">
-            <template slot="title">
-              <i class="fa fa-database"></i>
-              <span slot="title">数据表格</span>
-            </template>
-            <el-menu-item index="/basicTable">
-              <i class="fa fa-hdd-o"></i>
-              <span slot="title">基础表格</span>
-            </el-menu-item>
-            <el-menu-item index="/editableTable">
-              <i class="fa fa-bars"></i>
-              <span slot="title">可编辑表格</span>
-            </el-menu-item>
-          </el-submenu>
-
-          <el-submenu index="9">
-            <template slot="title">
-              <i class="fa fa-file"></i>
-              <span slot="title">页面</span>
-            </template>
-            <el-menu-item index="/login">
-              <i class="fa fa-mouse-pointer"></i>
-              <span slot="title">登录页</span>
-            </el-menu-item>
-            <el-menu-item index="/lockScreen">
-              <i class="fa fa-cogs"></i>
-              <span slot="title">锁屏</span>
-            </el-menu-item>
-          </el-submenu>
-        </el-menu-item-group>
+        <el-submenu index="9">
+          <template slot="title">
+            <i class="fa fa-file"></i>
+            <span slot="title">页面</span>
+          </template>
+          <el-menu-item index="/login">
+            <i class="fa fa-mouse-pointer"></i>
+            <span slot="title">登录页</span>
+          </el-menu-item>
+          <el-menu-item index="/lockScreen">
+            <i class="fa fa-cogs"></i>
+            <span slot="title">锁屏</span>
+          </el-menu-item>
+        </el-submenu>
       </el-menu>
     </aside>
     <el-row class="content-wrapper" :style="{paddingLeft: collapsed?'64px':'210px'}">
