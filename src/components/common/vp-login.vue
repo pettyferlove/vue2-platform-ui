5ac9e92682b9ddd930ea01f4ddd6e02d5ddb4b53
Pettyfer <pettyferlove@live.cn>
2017-11-03 13:58:58 +0800

    :memo:全屏代码

--- src/components/common/vp-login.vue
+++ src/components/common/vp-login.vue
@@ -1,26 +1,22 @@
 <template>
-  <el-card :body-style="{ padding: '0px' }">
-    <el-row class="vp-login vp-panel">
-      <el-col :span="10" class="login-form">
+  <el-row class="vp-login">
+    <el-col class="login-form">
       <div class="login-header">
         <img src="../../assets/img/logo.png" height="80" alt="">
         <p><span>VUE</span>PettyDev</p>
       </div>
+      <h3>登陆系统</h3>
       <div class="login-form">
-        <el-input v-model="username" placeholder="please enter username"></el-input>
-        <el-input v-model="password" type="password" placeholder="Please enter password"></el-input>
-        <router-link to="/"><el-button type="primary">Login</el-button></router-link>
+        <el-input v-model="username" placeholder="请输入用户名"></el-input>
+        <el-input v-model="password" type="password" placeholder="请输入密码"></el-input>
+        <router-link to="/"><el-button type="primary">登陆</el-button></router-link>
       </div>
       <div class="login-footer">
-        <el-checkbox v-model="remenber">Remenber</el-checkbox>
-        <span class="forget"><a href="#">Forget Password</a></span>
+        <el-checkbox v-model="remenber">记住我</el-checkbox>
+        <span class="forget"><a href="#">忘记密码</a></span>
       </div>
-      </el-col>
-      <el-col :span="14" class="login-ad">
-      <span class="photo-author"></span>
-      </el-col>
-    </el-row>
-  </el-card>
+    </el-col>
+  </el-row>
 </template>
 <script>
   export default {
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="login">
-    <vp-bg-canvas></vp-bg-canvas>
+    <div class="login-main vertical-align">
+      <div class="login-content vertical-align">
+        <div class="page-brand vertical-align-middle">
+          <span class="login-title">PlatformUI</span>
+          <h2>基于Vue2+ElementUI 开发的组件库</h2>
+          <h4><i class="fa fa-github"><a href="https://github.com/pettyferlove/vue2-platform-ui" style="color: white"> Github</a></i></h4>
+        </div>
+      </div>
+    </div>
     <vp-login>
     </vp-login>
   </div>
