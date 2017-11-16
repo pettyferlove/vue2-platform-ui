d95c349cf09ad5d64892b22839df7ca313e18ad8
Pettyfer <pettyferlove@live.cn>
2017-11-16 22:33:07 +0800

    vue-cli update

--- src/components/common/vp-breadcrumb-nav.vue
+++ src/components/common/vp-breadcrumb-nav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vp-breadcrumb-nav">
-    <el-breadcrumb separator="/">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item v-for="item in breadcrumbList" :to="item.path" :key="item.name">{{item.title}}</el-breadcrumb-item>
     </el-breadcrumb>
   </div>
