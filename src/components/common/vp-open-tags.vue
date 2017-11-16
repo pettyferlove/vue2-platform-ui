b16622173d7ca203ba640879f86b8d7ebbe9101b
Pettyfer <pettyferlove@live.cn>
2017-11-16 22:17:45 +0800

    vue-cli update

--- src/components/common/vp-open-tags.vue
+++ src/components/common/vp-open-tags.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <el-tag color="white">标签一</el-tag>
-    <el-tag>标签二</el-tag>
-    <el-tag>标签三</el-tag>
-    <el-tag>标签四</el-tag>
-    <el-tag>标签五</el-tag>
+    <el-tag color="white" closable type="info"><span class="ivu-tag-dot-inner"></span>仪表盘</el-tag>
+    <el-tag color="white" closable type="info"><span class="ivu-tag-dot-inner"></span>组件</el-tag>
+    <el-tag color="white" closable type="default"><span class="ivu-tag-dot-inner"  style="background: #409EFF"></span>面板</el-tag>
+    <el-tag color="white" closable type="info"><span class="ivu-tag-dot-inner"></span>编辑器</el-tag>
+    <el-tag color="white" closable type="info"><span class="ivu-tag-dot-inner"></span>图片列表</el-tag>
   </div>
 </template>
 
 <style>
-
+  .ivu-tag-dot-inner {
+    display: inline-block;
+    width: 12px;
+    height: 12px;
+    margin-right: 8px;
+    border-radius: 50%;
+    background: #e9eaec;
+    position: relative;
+    top: 1px;
+  }
 </style>
 
 <script>
