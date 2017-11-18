20e5c7d64ceb61e4cfd85da46659c61be1c04327
Pettyfer <pettyferlove@live.cn>
2017-11-18 10:02:53 +0800

    vue-cli update

--- src/components/common/vp-open-tags.vue
+++ src/components/common/vp-open-tags.vue
@@ -1,17 +1,6 @@
 <template>
-  <div class="vp-open-tags">
+  <div ref="scrollBody" class="vp-open-tags" @mousewheel="mouseHandler">
     <div class="vp-open-tags-close">
-<!--      <el-dropdown size="mini" split-button type="primary">
-        超小尺寸
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>黄金糕</el-dropdown-item>
-          <el-dropdown-item>狮子头</el-dropdown-item>
-          <el-dropdown-item>螺蛳粉</el-dropdown-item>
-          <el-dropdown-item>双皮奶</el-dropdown-item>
-          <el-dropdown-item>蚵仔煎</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>-->
-
       <el-dropdown size="mini" placement="top">
         <el-button size="mini" type="primary">
           关闭操作<i class="el-icon-arrow-down el-icon--right"></i>
@@ -23,7 +12,7 @@
         </el-dropdown-menu>
       </el-dropdown>
     </div>
-    <div class="vp-open-tags-body">
+    <div ref="scrollTags" class="vp-open-tags-body" :style="{left: tagBodyLeft + 'px'}">
       <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>仪表盘
       </el-tag>
       <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>组件
@@ -35,6 +24,35 @@
       </el-tag>
       <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>图片列表
       </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>仪表盘
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>组件
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="default"><span class="tag-dot-inner"
+                                                                                        style="background: #409EFF"></span>面板
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>编辑器
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>图片列表
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>仪表盘
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>组件
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="default"><span class="tag-dot-inner"
+                                                                                        style="background: #409EFF"></span>面板
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>编辑器
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>图片列表
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>仪表盘
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="info"><span class="tag-dot-inner"></span>组件
+      </el-tag>
+      <el-tag class="vp-open-tags-body-tag" color="white" closable type="default"><span class="tag-dot-inner"
+                                                                                        style="background: #409EFF"></span>面板
+      </el-tag>
     </div>
   </div>
 </template>
@@ -45,7 +63,35 @@
 
 <script>
   export default {
-    name: 'VpOpenTags'
+    name: 'VpOpenTags',
+    data () {
+      return {
+        currentPageName: this.$route.name,
+        tagBodyLeft: 0,
+        currentScrollBodyWidth: 0,
+        refsTag: [],
+        tagsCount: 1
+      }
+    },
+    methods: {
+      mouseHandler (e) {
+        let left = 0
+        if (e.wheelDelta > 0) {
+          left = Math.min(0, this.tagBodyLeft + e.wheelDelta)
+        } else {
+          if (this.$refs.scrollBody.offsetWidth - 110 < this.$refs.scrollTags.offsetWidth) {
+            if (this.tagBodyLeft < -(this.$refs.scrollTags.offsetWidth - this.$refs.scrollBody.offsetWidth + 110)) {
+              left = this.tagBodyLeft
+            } else {
+              left = Math.max(this.tagBodyLeft + e.wheelDelta, this.$refs.scrollBody.offsetWidth - this.$refs.scrollTags.offsetWidth - 110)
+            }
+          } else {
+            this.tagBodyLeft = 0
+          }
+        }
+        this.tagBodyLeft = left
+      }
+    }
   }
 </script>
 
