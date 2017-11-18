29ae7325494dcda7cb8a7b88ff438237cde97a65
Pettyfer <pettyferlove@live.cn>
2017-11-18 14:10:32 +0800

    vue-cli update

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -4,7 +4,6 @@ util.updateOpenTags = function (_state, _tag) {
   // 判断该页面是否已经打开过
   let isOpen = false
   _state.forEach(item => {
-    console.log(item)
     if (item.name === _tag.name) {
       isOpen = true
     }
@@ -12,8 +11,6 @@ util.updateOpenTags = function (_state, _tag) {
   if (!isOpen) {
     _state.push(_tag)
   }
-  console.log(_state)
-  console.log(_tag)
 }
 
 export default util
