9a3802a7e1e190326d8ae56f1b42832bbb43f3b2
Pettyfer <pettyferlove@live.cn>
2017-11-23 20:22:08 +0800

    mini-analysis-text component

--- src/components/user/vp-user-welcome.vue
+++ src/components/user/vp-user-welcome.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="pageHeader___qgeO0">
-    <div class="breadcrumb___2WwSj ant-breadcrumb"><span><span class="ant-breadcrumb-link"><a
-      href="#/">首页</a></span><span class="ant-breadcrumb-separator">/</span></span><span><span
-      class="ant-breadcrumb-link"><a href="#/dashboard">Dashboard</a></span><span
-      class="ant-breadcrumb-separator">/</span></span><span><span class="ant-breadcrumb-link"><span
-      to="/dashboard/workplace">工作台</span></span><span class="ant-breadcrumb-separator">/</span></span></div>
-    <div class="detail___3j7je">
-      <div class="main___3aXyE">
-        <div class="row___1YNAY"></div>
-        <div class="row___1YNAY">
-          <div class="content___YoaiY">
-            <div class="pageHeaderContent___1laGY">
-              <div class="avatar___2k6Px"><span class="ant-avatar ant-avatar-lg ant-avatar-circle ant-avatar-image"><img
-                src="https://gw.alipayobjects.com/zos/rmsportal/lctvVCLfRpYCkYxAsiVQ.png"></span></div>
-              <div class="content___2WKoV">
-                <div class="contentTitle___2l2WI">早安，曲丽丽，祝你开心每一天！</div>
+  <div class="user-welcome">
+    <div class="user-welcome-detail">
+      <div class="user-welcome-detail-main">
+        <div class="user-welcome-detail-main-row">
+          <div class="user-welcome-detail-main-row-content">
+            <div class="header-content">
+              <div class="avatar">
+                <span class="avatar-circle">
+                  <img src="../../assets/img/user-petty.jpg" height="72">
+                </span>
+              </div>
+              <div class="content">
+                <div class="content-title">早安，曲丽丽，祝你开心每一天！</div>
                 <div>交互专家 | 蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED</div>
               </div>
             </div>
           </div>
-          <div class="extraContent___2lyIs">
-            <div class="pageHeaderExtra___2BNcQ">
+          <div class="user-welcome-detail-main-row-extra">
+            <div class="extra-content">
               <div><p>项目数</p>
                 <p>56</p></div>
               <div><p>团队内排名</p>
@@ -40,7 +37,10 @@
 </style>
 
 <script>
+  import ElRow from 'element-ui/packages/row/src/row'
+
   export default {
+    components: {ElRow},
     name: 'VpUserWelcome'
   }
 </script>
