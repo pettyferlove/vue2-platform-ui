245e8cc6a1ba7ae4f5dbb85865035cd72eae4cc0
Pettyfer <pettyferlove@live.cn>
2017-11-25 17:04:32 +0800

    mini-analysis-text component

--- src/components/user/vp-user-welcome.vue
+++ src/components/user/vp-user-welcome.vue
@@ -6,31 +6,35 @@
     </div>
     <div class="user-welcome-detail">
       <div class="user-welcome-detail-main">
-        <div class="user-welcome-detail-main-row">
-          <div class="user-welcome-detail-main-row-content">
-            <div class="header-content">
-              <div class="avatar">
+        <el-row :gutter="16">
+          <el-col :lg="12" :md="12">
+            <div class="user-welcome-detail-main-content">
+              <div class="header-content">
+                <div class="avatar">
                 <span class="avatar-circle">
                   <img src="../../assets/img/user-petty.jpg" height="72">
                 </span>
-              </div>
-              <div class="content">
-                <div class="content-title">早安，曲丽丽，祝你开心每一天！</div>
-                <div class="content-detail">Java开发工程师 | 某某公司-某某部门-某某项目组-Java开发工程师</div>
+                </div>
+                <div class="content">
+                  <div class="content-title">早安，曲丽丽，祝你开心每一天！</div>
+                  <div class="content-detail">Java开发工程师 | 某某公司-某某部门-某某项目组-Java开发工程师</div>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="user-welcome-detail-main-row-extra">
-            <div class="extra-content">
-              <div><p>项目数</p>
-                <p>56</p></div>
-              <div><p>团队内排名</p>
-                <p>8<span> / 24</span></p></div>
-              <div><p>项目访问</p>
-                <p>2,223</p></div>
+          </el-col>
+          <el-col :lg="12" :md="12">
+            <div class="user-welcome-detail-main-extra">
+              <div class="extra-content">
+                <div><p>项目数</p>
+                  <p>56</p></div>
+                <div><p>团队内排名</p>
+                  <p>8<span> / 24</span></p></div>
+                <div><p>项目访问</p>
+                  <p>2,223</p></div>
+              </div>
             </div>
-          </div>
-        </div>
+          </el-col>
+        </el-row>
       </div>
     </div>
   </div>
