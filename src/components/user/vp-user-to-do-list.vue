00cb96d2034ce2241ec52085b7d40d44bccb6cfc
Pettyfer <pettyferlove@live.cn>
2017-12-01 16:31:25 +0800

    待办列表

---
+++ src/components/user/vp-user-to-do-list.vue
@@ -0,0 +1,19 @@
+<template>
+  <vp-panel title="待处理事项" class="vp-user-to-do-list">
+
+  </vp-panel>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  import VpPanel from '../common/vp-panel.vue'
+
+  export default {
+    components: {VpPanel},
+    name: 'VpUserToDoList'
+  }
+</script>
+
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,6 +7,16 @@
     </el-row>
 
     <el-row :gutter="16">
+      <el-col :lg="9" :md="12">
+        <vp-user-to-do-list></vp-user-to-do-list>
+      </el-col>
+
+      <el-col :lg="15" :md="12">
+
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="16">
       <el-col :lg="16" :md="12">
         <vp-user-dynamic></vp-user-dynamic>
       </el-col>
@@ -21,13 +31,28 @@
   import VpUserWelcome from '../components/user/vp-user-welcome.vue'
   import VpUserIndexNum from '../components/user/vp-user-index-num.vue'
   import VpUserDynamic from '../components/user/vp-user-dynamic.vue'
+  import VpUserToDoList from '../components/user/vp-user-to-do-list.vue'
 
   export default {
     components: {
+      VpUserToDoList,
       VpUserDynamic,
       VpUserIndexNum,
-      VpUserWelcome},
-    name: 'Home'
+      VpUserWelcome
+    },
+    name: 'Home',
+    data () {
+      return {
+        dataWeather: {
+          location: 'Beijing',
+          weather: 'cloudy',
+          temprature: '30°',
+          humidity: '56%',
+          percip: '1.50in',
+          winds: '10mph'
+        }
+      }
+    }
   }
 </script>
 
