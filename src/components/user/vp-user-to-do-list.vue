7b76b61704d201fe976d216abd741302840e1824
Pettyfer <pettyferlove@live.cn>
2017-12-01 17:26:39 +0800

    待办列表

--- src/components/user/vp-user-to-do-list.vue
+++ src/components/user/vp-user-to-do-list.vue
@@ -1,6 +1,12 @@
 <template>
   <vp-panel title="待处理事项" class="vp-user-to-do-list">
+    <petty-scroll>
+      <div style="height: 300px">
+        <div style="height: 1000px;">
 
+        </div>
+      </div>
+    </petty-scroll>
   </vp-panel>
 </template>
 
@@ -10,9 +16,12 @@
 
 <script>
   import VpPanel from '../common/vp-panel.vue'
+  import PettyScroll from '../../../node_modules/petty-scroll/src/petty-scroll/main.vue'
 
   export default {
-    components: {VpPanel},
+    components: {
+      PettyScroll,
+      VpPanel},
     name: 'VpUserToDoList'
   }
 </script>
