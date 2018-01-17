76f7b448e6cb35c1859296dc9f845bcb9fc096dd
pettyferlove@live.cn <pettyferlove@live.cn>
2018-01-17 15:01:55 +0800

    init

--- src/pages/BasicForm.vue
+++ src/pages/BasicForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="vp-content">
-    <el-card :body-style="{ padding: '0px' }" style="padding: 30px 20% 30px 20%;">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="活动名称">
+    <vp-panel title="基础表单">
+      <el-form  style="padding: 30px 20% 30px 20%;" ref="form" :model="form" label-width="80px">
+        <el-form-item label="标题">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
         <el-form-item label="活动区域">
@@ -20,21 +20,17 @@
             <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
           </el-col>
         </el-form-item>
-        <el-form-item label="即时配送">
-          <el-switch v-model="form.delivery"></el-switch>
-        </el-form-item>
         <el-form-item label="活动性质">
           <el-checkbox-group v-model="form.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-            <el-checkbox label="地推活动" name="type"></el-checkbox>
-            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+            <el-checkbox label="联谊活动" name="type"></el-checkbox>
+            <el-checkbox label="部门活动" name="type"></el-checkbox>
+            <el-checkbox label="户外活动" name="type"></el-checkbox>
           </el-checkbox-group>
         </el-form-item>
-        <el-form-item label="特殊资源">
+        <el-form-item label="目标公开">
           <el-radio-group v-model="form.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
+            <el-radio label="公开"></el-radio>
+            <el-radio label="不公开"></el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="活动形式">
@@ -45,7 +41,7 @@
           <el-button>取消</el-button>
         </el-form-item>
       </el-form>
-    </el-card>
+    </vp-panel>
   </div>
 </template>
 
@@ -54,7 +50,10 @@
 </style>
 
 <script>
+  import VpPanel from '../components/common/vp-panel'
+
   export default {
+    components: {VpPanel},
     name: 'BasicForm',
     data () {
       return {
@@ -63,7 +62,6 @@
           region: '',
           date1: '',
           date2: '',
-          delivery: false,
           type: [],
           resource: '',
           desc: ''
--- src/pages/StepForm.vue
+++ src/pages/StepForm.vue
@@ -1,5 +1,22 @@
 <template>
+  <div class="vp-content">
+    <vp-panel title="分布表单">
+      <el-steps :active="2" align-center>
+        <el-step title="步骤1" description="这是一段很长很长很长的描述性文字">
 
+        </el-step>
+        <el-step title="步骤2" description="这是一段很长很长很长的描述性文字">
+
+        </el-step>
+        <el-step title="步骤3" description="这是一段很长很长很长的描述性文字">
+
+        </el-step>
+        <el-step title="步骤4" description="这是一段很长很长很长的描述性文字">
+
+        </el-step>
+      </el-steps>
+    </vp-panel>
+  </div>
 </template>
 
 <style>
@@ -7,7 +24,10 @@
 </style>
 
 <script>
+  import VpPanel from '../components/common/vp-panel'
+
   export default {
+    components: {VpPanel},
     name: 'StepForm'
   }
 </script>
