36728ad562d2bf4c82db846119aefe40503efb8c
pettyferlove@live.cn <pettyferlove@live.cn>
2018-01-30 16:30:10 +0800

    init

--- src/pages/BasicForm.vue
+++ src/pages/BasicForm.vue
@@ -3,38 +3,38 @@
     <vp-panel title="基础表单">
       <el-form  style="padding: 30px 20% 30px 20%;" ref="form" :model="form" label-width="80px">
         <el-form-item label="标题">
-          <el-input v-model="form.name"></el-input>
+          <el-input size="small" v-model="form.name"></el-input>
         </el-form-item>
         <el-form-item label="活动区域">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-select size="small" v-model="form.region" placeholder="请选择活动区域">
             <el-option label="区域一" value="shanghai"></el-option>
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="活动时间">
           <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            <el-date-picker size="small" type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
           </el-col>
           <el-col class="line" :span="2">-</el-col>
           <el-col :span="11">
-            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            <el-time-picker size="small" type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
           </el-col>
         </el-form-item>
         <el-form-item label="活动性质">
-          <el-checkbox-group v-model="form.type">
+          <el-checkbox-group size="small" v-model="form.type">
             <el-checkbox label="联谊活动" name="type"></el-checkbox>
             <el-checkbox label="部门活动" name="type"></el-checkbox>
             <el-checkbox label="户外活动" name="type"></el-checkbox>
           </el-checkbox-group>
         </el-form-item>
         <el-form-item label="目标公开">
-          <el-radio-group v-model="form.resource">
+          <el-radio-group size="small" v-model="form.resource">
             <el-radio label="公开"></el-radio>
             <el-radio label="不公开"></el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="活动形式">
-          <el-input type="textarea" v-model="form.desc"></el-input>
+          <el-input size="small" type="textarea" v-model="form.desc"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">立即创建</el-button>
--- src/pages/Panels.vue
+++ src/pages/Panels.vue
@@ -2,65 +2,55 @@
   <div class="panels vp-content">
     <el-row :gutter="15">
       <el-col :span="12">
-        <vp-panel title="Panel Title">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
-        <vp-panel title="Panel Title" bg-color="#2c2e2f">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" bg-color="#2c2e2f">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
-        <vp-panel title="Panel Title" bg-color="#41b883">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" bg-color="#41b883">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
-        <vp-panel title="Panel Title" bg-color="#1d8ce0">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" bg-color="#1d8ce0">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
 
       <el-col :span="12">
         <vp-panel>
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
         <vp-panel>
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
 
       <el-col :span="12">
-        <vp-panel title="Panel Title" hd-color="#40bbea">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" hd-color="#40bbea">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
-        <vp-panel title="Panel Title" hd-color="#cc3f44">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" hd-color="#cc3f44">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
-        <vp-panel title="Panel Title" hd-color="#8dc63f">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" hd-color="#8dc63f">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
       <el-col :span="12">
-        <vp-panel title="Panel Title" hd-color="#ffba00">
-          Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-          Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least.
+        <vp-panel title="默认面板" hd-color="#ffba00">
+          通过 vp-panel 可以很简单地为面板加入一个标题容器。你也可以通过设置 title="" 属性，添加一个预定义样式的标题。 如果需要设置标题颜色请添加bg-color=""。
         </vp-panel>
       </el-col>
     </el-row>
