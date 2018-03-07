20aa0b4c99ac3ad41d2d8924c036b0fbe15feded
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-07 20:13:56 +0800

    Login Form Fix

--- src/components/common/vp-login.vue
+++ src/components/common/vp-login.vue
@@ -1,11 +1,11 @@
 <template>
   <el-row class="vp-login">
-    <el-col class="login-form">
+    <div class="login-form">
       <div class="login-header">
         <img src="../../assets/img/logo.png" height="80" alt="">
         <p><span>VUE</span>PettyDev</p>
+        <h3>登陆系统</h3>
       </div>
-      <h3>登陆系统</h3>
       <el-form status-icon ref="loginForm" :model="loginForm" :rules="loginRules" class="login-body">
         <el-form-item prop="username">
           <el-input size="medium" v-model="loginForm.username" type="text" placeholder="请输入用户名"/>
@@ -21,7 +21,7 @@
         <el-checkbox v-model="remenber">记住我</el-checkbox>
         <span class="forget"><a href="#">忘记密码</a></span>
       </div>
-    </el-col>
+    </div>
   </el-row>
 </template>
 <script>
